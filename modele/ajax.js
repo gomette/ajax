725d6048f399b41b62d4810ea5a5e5b04b96a0fb
Aflokkat_user_19 <luftlaura@gmail.com>
2020-02-05 11:55:19 +0100

    peuplement de la ville

--- modele/ajax.js
+++ modele/ajax.js
@@ -20,9 +20,9 @@ $(document).ready(function() {
         //data: { region: region },
 
         success: function(retour) {
-            var ret = JSON.parse(retour);
-            for (var i = 0; i < ret.length; i++) {
-                $('#dep').append("<option value=" + ret[i].iddep + ">" + ret[i].nomdep + "</select>");
+            var retu = JSON.parse(retour);
+            for (var i = 0; i < retu.length; i++) {
+                $('#ville').append("<option value=" + retu[i].iddep + ">" + retu[i].nomvilles + "</select>");
             }
         }
     })
