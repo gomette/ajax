1c158bef244a48daa8b377895b50b5f3297fc1df
Aflokkat_user_19 <luftlaura@gmail.com>
2020-02-06 08:50:26 +0100

    change

--- modele/ajax.js
+++ modele/ajax.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
         //data: { region: region },
 
         success: function(retour) {
-            alert(retour);
+            // alert(retour);
             var ret = JSON.parse(retour);
 
+            $('#dep').append("<option value=''>" + "--choisir un departement--" + "</option>");
             for (var i = 0; i < ret.length; i++) {
 
                 $('#dep').append("<option value=" + ret[i].iddep + ">" + ret[i].nomdep + "</option>");
             }
         }
     })
+
+
 });
--- modele/selectville.php
+++ modele/selectville.php
@@ -4,19 +4,19 @@ require_once("connexion.php");
 
 function selectville()
 {
-     if(isset($_POST['dep']))
-     {
+    //  if(isset($_POST['dep']))
+    //  {
 
-         $id = $_POST['dep'];
-     }
+    //      $id = $_POST['dep'];
+    //  }
 
-    var_dump($id);
+    // var_dump($id);
     $servername = "localhost";
     $username = "root";
     $password = "";
     $conn = new PDO("mysql:host=$servername;dbname=corse", $username, $password);
     $n = $conn->query("SELECT nomvilles FROM villes INNER JOIN dep ON dep.iddep = villes.iddep WHERE dep.iddep = $id" );
-var_dump($n);
+// var_dump($n);
     $r = $n->fetchall();
     echo json_encode($r);
 }
